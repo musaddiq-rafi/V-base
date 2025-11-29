64da769762a6143d43f36d1faf9893021dde8a19
Raiyan <raiyanmuhtasim@gmail.com>
2025-11-29 18:48:33 +0600

    added convex clerk auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
-import ConvexClientProvider from "./components/ConvexClientProvider";
-import StoreUserEffect from "./components/StoreUserEffect";
+
 import "./globals.css";
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,17 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <ConvexClientProvider>
-            <StoreUserEffect />
             {children}
           </ConvexClientProvider>
         </body>
       </html>
-    </ClerkProvider>
   );
 }
